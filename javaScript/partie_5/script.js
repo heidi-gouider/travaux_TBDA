a85f97bca10208407c8196d740f9cc9118565b78
heidi <gouider.heidi@gmail.com>
2023-05-04 15:25:01 +0200

    exercices Js partie 5

---
+++ javaScript/partie_5/script.js
@@ -0,0 +1,16 @@
+//je creer différentes boîtes de dialogues
+//c'est à dire que je vais demander à l'utilisateur d'entrer des information
+//ensuite je vais demander au programme de manipuler les informations données
+var nom = prompt("Indiquez votre nom puis votre prénom");
+    alert (nom);
+
+var nb1 = prompt(" Entrez un 1er nombre");
+var nb2 = prompt(" Entrez un 2eme nombre");
+var produit = nb1*nb2; 
+alert (produit); 
+
+
+var tempCel = prompt("Entrez la température");
+var tempFar = tempCel*9/5 + 32;
+alert (tempFar, "Fahrenheit");
+
