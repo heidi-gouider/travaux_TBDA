3b3cf83f51aa17363a7b37205e7980cd9bec28e6
heidi <gouider.heidi@gmail.com>
2023-05-04 15:27:15 +0200

    ajout de commentaire

--- javaScript/partie_5/script.js
+++ javaScript/partie_5/script.js
@@ -1,6 +1,6 @@
 //je creer différentes boîtes de dialogues
-//c'est à dire que je vais demander à l'utilisateur d'entrer des information
-//ensuite je vais demander au programme de manipuler les informations données
+//c'est à dire que je vais demander à l'utilisateur d'entrer des informations
+//ensuite je vais demander au programme de manipuler les informations données et de les renvoyer à l'utilisateur
 var nom = prompt("Indiquez votre nom puis votre prénom");
     alert (nom);
 
