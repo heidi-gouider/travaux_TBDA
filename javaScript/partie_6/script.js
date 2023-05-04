8b876ff217d4327732e74fd837d3f4f944f68030
heidi <gouider.heidi@gmail.com>
2023-05-04 17:47:06 +0200

    partie 6

---
+++ javaScript/partie_6/script.js
@@ -0,0 +1,19 @@
+var a = "100", b = 100, c = 1.00, d = true;
+
+//document.write("Ceci est une chaîne de caractères :");
+
+var string = "Ceci est une chaîne de caractères :";
+//document.write(string);
+var conca = string + a;
+document.write(conca);
+var decr = b--;
+console.log (decr);
+var ajout = c += a;
+console.log (ajout);
+var inv = !d;
+console.log(inv);
+
+
+
+
+
