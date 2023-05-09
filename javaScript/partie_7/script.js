9780c98457347e44956cc27c1b4ff3f94a423540
heidi <gouider.heidi@gmail.com>
2023-05-09 13:01:43 +0200

    partie_7

--- javaScript/partie_7/script.js
+++ javaScript/partie_7/script.js
@@ -1,33 +1,43 @@
-var nb = Number (prompt("Entrez un nombe"));
+// var nb = Number (prompt("Entrez un nombe"));
 
-if (nb %= 2 == 0) {
-    alert("nombre pair");
-}
-else {
-    alert("nombre impair");
-}
+// if (nb %= 2 == 0) {
+//     alert("nombre pair");
+// }
+// else {
+//     alert("nombre impair");
+// }
 
-var birth = Number (prompt("Entrez votre année de niassance:"));
-var date = 2023;
+// var birth = Number (prompt("Entrez votre année de niassance:"));
+// var date = 2023;
 
-if (date - birth >= 18) {
-    alert("Vous êtes majeur");
-}
-else {
-    alert("Vous êtes mineur");
-}
+// if (date - birth >= 18) {
+//     alert("Vous êtes majeur");
+// }
+// else {
+//     alert("Vous êtes mineur");
+// }
 
-//ici je vais utiliser deux méthodes différentes(pour m'amuser;)
-//la première avec l'instruction switch
-//la deuxième avec soit une condition multiple soit une condition simple
-var nb1 = prompt("Entrez un premier nombre");
-var nb2 = prompt("Entrez un deuxième nombre");
+var nb1 = Number (prompt("Entrez un premier nombre"));
+var nb2 = Number (prompt("Entrez un deuxième nombre"));
+//Pour les valeurs des deux variables précédentes, j'ai utilisé le constructeur Number
+//pour éviter que la réponse soit une chaine de caractère. Ce constructeur permet de la convertir en nombe
 var operateur = prompt("Entrez un opérateur");
 
 switch (operateur){
     case "+":
         alert(nb1 + nb2);
         break;
+    case "-":
+        alert(nb1 - nb2);
+        break;
+    case "*":
+        alert(nb1 * nb2);
+        break;
+    case "/":
+        alert(nb1 / nb2);
+        break;
+    default :
+        alert("erreur");        
 }
 
 
