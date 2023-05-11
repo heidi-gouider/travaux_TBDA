9274da347db84ec23b0a18ce13ab73162c4d9f58
heidi <gouider.heidi@gmail.com>
2023-05-11 13:57:43 +0200

    tableau

---
+++ javaScript/tableaux/script.js
@@ -0,0 +1,21 @@
+var saisie = prompt("Entrer le nombres d'elements");
+var tableau = new Array(saisie);
+var taille = tableau.length;
+
+// tableau = [taille];
+console.log(tableau);
+
+for(i=0; i<=taille; i++){
+    var elements = prompt("entrer l'element n°" + (i+1));
+
+    // console.log(elements);
+}
+    console.log(elements);
+
+
+// function afficheTableau(tab) {
+//     var tab = document.createElement("board");
+//     var newContent = document.create
+//     document.body.appendChild(image);
+// }
+// afficheTableau("tab");
