25a340b2b8ff0f52c08140e65b76866e9d6e75ce
heidi gouider <gouider.heidi@gmail.com>
2023-05-12 16:40:22 +0200

    ENREGISTREMENT DES MODIF SUR EXER EVAL

--- evaluation_js/exercice1/script.js
+++ evaluation_js/exercice1/script.js
@@ -4,18 +4,51 @@
 
 // let age = document.getElementById("age");
 // addEventListener()
-    // for(i=0, i<=age, i++){
+// for(i=0, i<=age, i++){
 
-    // }
+// }
 
-    let age 
 let ages = "age";
 i = 0;
-    
+
 while (ages !== "") {
-    ages = prompt("Saisissez l'age N°" + (i + 1) + "\n ou Clic sur Annuler pour arrêter la saisie.");
+    ages = prompt("Saisissez l'age N°" + (i + 1) + "\n ou Clic sur Annuler pour arrêter la saisie.") * 1;
     i++;
-        // if (prenom == "") {
-            // console.log(prenom + i-1 + "");
+
+    console.log(ages + i - 1);
 }
-console.log(ages+ i-1);
+
+
+
+
+// lorsque l'utilisateur valide un champ vide je lui indique le nombre
+// total de chaque catégorie entrée
+// let ages = jeune, moyen, vieux;
+
+// function totalCat(nbJeune, nbMoyen, nbVieux) {
+//     // let ages = jeune, moyen, vieux;
+
+//     if (ages > 40) {
+//         nbVieux = N *;
+//     }
+//     else if (ages <= 40 && ages >= 20) {
+//         ages = moyen;
+//     }
+//     else {
+//         ages = jeune;
+//     }
+//     console.log("il y a " + jeune + "jeune" + "\n" + "il y a " + moyen + "moyen" + "\n" + "il y a " + vieux + "vieux");
+// }
+
+// totalCat(ages);
+// console.log(ages + i - 1);
+
+// const input = document.querySelector('input');
+
+// input.onclick = function() {
+//   let name = prompt('Quel est votre nom&nbsp;?');
+//   alert('Salut ' + name + ', sympa de vous voir&nbsp;!');
+// }
+
+
+
