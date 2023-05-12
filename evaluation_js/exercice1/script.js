810606e1858cfb4f64fedaad54fb5cf25ca2d4f0
heidi <gouider.heidi@gmail.com>
2023-05-12 11:47:00 +0200

    exercice eval

---
+++ evaluation_js/exercice1/script.js
@@ -0,0 +1,11 @@
+//  je crée une boite de dialogue qui de mande à l'utilisateur
+// différents âges. Pour ça je crée un element = la balise input
+// avec l'attribut id
+
+// let age = document.getElementById("age");
+// addEventListener()
+    // for(i=0, i<=age, i++){
+
+    // }
+
+    let age 
--- javaScript/tableaux/script.js
+++ javaScript/tableaux/script.js
@@ -1,11 +1,13 @@
 // je demande à l'utilisateur d'indi
 // var saisie = prompt("Entrer le nombres d'elements");
 // var tableau = new Array(saisie, elements ='');
-var tableau = new Array (taille = prompt("Entrer le nombres de lignes")*1, elements = '');
+
+
+let tableau = new Array (taille = prompt("Entrer le nombres de lignes")*1, elements = '');
 console.log(tableau);
 
-for(i=0; i<=tableau.length; i++){
-    var valeurElement = prompt("entrer l'element n°" + (i+1));
+for(i=0; i<=taille; i++){
+    let valeurElement = prompt("entrer l'element n°" + (i+1));
     elements+= valeurElement + taille [i]; //"\n";
     // elements+= valeurElement [i]; //"\n";
 
