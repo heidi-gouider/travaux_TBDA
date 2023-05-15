e6f8081695719be0059a00a16a4331624fd2b740
heidi <gouider.heidi@gmail.com>
2023-05-15 13:15:19 +0200

    enregistrement des modifs d'éval

--- evaluation_js/ecercice3/script.js
+++ evaluation_js/ecercice3/script.js
@@ -1,18 +1,20 @@
 let prenom = prompt("Saisissez un prénom :");
 let tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
 
-// pour parcourie le tableau j'utilise la boucle for et la fonction length()
-// la première question que je me pose est quelle méthode utiliser?
-//1/ pour trouver un element du tableau par sa valeur
-//2/ pour supprimer un element du tableau
-//3/ pour décaler les cases suivantes de cet element
-//4/ pour que la longueur du tableau rest la même et donc avoir une case vide
+// la première question que je me pose est quelle méthode utiliser ;)?
 
-//1/Pour chercher le prénom saisie par l'utilisateur dans mon tableau : la methode indexOf(indice) ou find(valeur)?
-let ok = tab.find(element => element == prenom);
+//1- pour trouver un element du tableau par sa valeur
+//2- pour supprimer un element du tableau
+//3- pour décaler les cases suivantes de cet element
+//4- pour que la longueur du tableau rest la même et donc avoir une case vide
+
+//1-Pour chercher le prénom saisie par l'utilisateur dans mon tableau : 
+// la methode indexOf(indice) pour rechercher l'indice ou find pour trouver la valeur)?
+// let ok = tab.find(element => element == prenom);
 //2/ j'utilise la méthode splice pour modifier un element choisi
 // if (ok == true){
-//     ind = tab.indexOf(prenom);
+    // ind = tab.indexOf(ok);
+// console.log(prenom);
 //     supr = tab.spicle(0, 1);
 
 // }
@@ -20,5 +22,16 @@ let ok = tab.find(element => element == prenom);
 //     console.log("erreur");
 // }
 
+
+// function nouveauTableau(tab, prenom){
+//     if (tab.indexOf(prenom) > -1){
+//         tab.splice(prenom);
+//         console.log(tab);
+//     }
+//     else if (tab.indexOf(prenom) === -1) {
+//         console.log("erreur")
+//     }
+// }
+// nouveauTableau(tab, prenom);
 
 // supr = tab.splice(prenom)
