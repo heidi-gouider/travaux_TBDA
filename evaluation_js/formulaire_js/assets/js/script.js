ac3e9a1f75c3738ea9154c6bca7845d49d0849ec
heidi gouider <gouider.heidi@gmail.com>
2023-05-23 12:40:08 +0200

    enreg. modif validation formulaire contact

--- evaluation_js/formulaire_js/assets/js/script.js
+++ evaluation_js/formulaire_js/assets/js/script.js
@@ -3,47 +3,80 @@
 
 /* je vais choisir la méthode querySelector pour selectionner le formulaire de contact par son identifiant ("#")
 pour me permettre par la suite d'acceder aux différents éléments grâce à leur name*/
-let form = document.querySelector("#contactForm");
+let form = document.querySelector("#contact");
+// document.forms("contact");
 
 /* je vais créer une écoute lorsque le bouton de validation est clické pour indiquer à l'utilisateur
 si un ou pls champs obligatoires sont vides 
 s'il manque une ou pls valeurs les données du formulaire ne seront pas envoyées
 */
 
-form.valider.addEventListener("click", function (e) {
-    e.preventDefault();
+form.valider.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // let inputs = document.getElementById("champs-obligatoires").getElementsByTagName("input");
+
+    // for (let i = 0; i < inputs.length; i++) {
+    //     if (inputs[i].validity.valueMissing)
+    //     inputs.style.border = " 1px solid red";
+
+    // }
+
+  
+/* je vais utiliser la propriété validity.valueMissing
+   pour vérifier si les champ de saisie ne sont pas vide et renvoyer un message si c'est le cas.*/
+
+
         if (form.prenom.validity.valueMissing) {
             // e.preventDefault();
             form.prenom.style.border = " 1px solid red";
             prenomMiss.textContent = "Ce champ est obligatoire";
+            // form.small.textContent = "Ce champ est obligatoire";
         
         }
-        else if (form.nom.validity.valueMissing) {
+        if (form.nom.validity.valueMissing) {
             // e.preventDefault();
             form.nom.style.border = " 1px solid red";
             nomMiss.textContent = "Ce champ est obligatoire";
 
         }
-        else if (form.feminin.validity.valueMissing || form.masculin.validity.valueMissing) {
+        if (form.feminin.validity.valueMissing || form.masculin.validity.valueMissing) {
             // e.preventDefault();
-            form.sexe.style.border = " 1px solid red";
+            // checkbox.style.border = " 1px solid red";
             sexeMiss.textContent = "Ce champ est obligatoire";
         }
-        else if (validEmail(form.email)==fasle){
-            // e.preventDefault();
-        }
+
+//je vérifie si la valeur saisi par l'utilisateur respecte l'expression régulière suivante
+
+        // let validEmail = function (email) {
+            let emailRegExp = new RegExp(
+            "^[a-zA-Z0-9;-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$", "g"
+            );
+
+        if(form.email != emailRegExp) {
+            form.email.style.border = "1px solid red";
+            validEmail.textContent = "email non valide";
+            // small.classList.add = "text-danger";
+            // return fasle;
+        } 
+        // };
+
+            
         else {
             form.valider.setCustomValidity("");
         }
+    
+
+
     });
 
     // form.valider.addEventListener("click", function (e) {
-    //     let formInput = document.getElementsByClassName('champ') [1];
-    //     let small = formInput.nextElementSibling;
-
+    //     // let formInput = document.getElementsByClassName('champ') [1];
+    //     // let small = formInput.nextElementSibling;
+    //     let inputs = document.getElementById("champs-obligatoires").getElementsByTagName("input");
     //     if (form.prenom.validity.valueMissing || form.nom.validity.valueMissing ||form.feminin.validity.valueMissing || form.masculin.validity.valueMissing ) {
     //         e.preventDefault();
-    //         // formInput.style.border = " 1px solid red";
+    //         form.style.border = " 1px solid red";
     //         small.innertHtml = "champ obligatoire";
     //         small.classList.add = "text-danger";
         
@@ -54,41 +87,40 @@ form.valider.addEventListener("click", function (e) {
     // });
 
 
-// form.annuler.addEventListener("click", function(event) {
-//     form.annuler.setCustomValidity("");
-// });
-
-/* je vais également m'assurer que la saisie de l'utilisateur soit correct
-Pour ça je vais utiliser une expression régulière(regex)*/
+/* je vais m'assurer que la saisie de l'utilisateur soit correct
+Pour ça je vais utiliser une expression régulière(regexp)*/
 // let verif = new RegExp("");
 
-form.email.addEventListener("change", function() {
-    validEmail(this);
-});
+    /* *********** TEST VALIDITE EMAIL *************/
 
-let validEmail = function (inputEmail) {
-    let emailRegExp = new RegExp(
-    "^[a-zA-Z0-9;-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$", "g"
-    );
+// form.email.addEventListener("change", function() {
+//     validEmail(this);
+// });
+
+// let validEmail = function (email) {
+//     let emailRegExp = new RegExp(
+//     "^[a-zA-Z0-9;-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$", "g"
+//     );
 
 //je vérifie si la valeur saisi par l'utilisateur respecte l'expression régulière(vrai ou fausse)
 //si l'email est valide les bordures de l'input seront vertes
 //sinon, un message apparaitra en rouge
 //je demande ensuite à la fonction de renvoyer l'information si vrai ou faux
-let testEmail = emailRegExp.test (inputEmail.value);
-let small = inputEmail.nextElementSibling;
-let borderInput = document.getElementsByClassName("champ")[1].style.border;
 
-if(testEmail == fasle) {
-    borderInput = "1px solid red";
-    small.innertHtml = "email non valide";
-    small.classList.add = "text-danger";
-    return fasle;
-}
-else {
-    return true;
-}
-};
+// let testEmail = emailRegExp.test (email.value);
+// let small = inputEmail.nextElementSibling;
+// let borderInput = document.getElementsByClassName("champ")[1].style.border;
+
+// if(testEmail != validEmail) {
+//     form.validEmail.style.border = "1px solid red";
+//     form.validEmail.innertHtml = "email non valide";
+    // small.classList.add = "text-danger";
+    // return fasle;
+// }
+// else {
+//     return true;
+// }
+// };
 //ensuite je fais en sorte que si la saisi est correct elle puisse égale est envoyé
 
 
