ec4ced1bc58d1c3b4a3ac94ad81fe3856e6bd6cc
heidi gouider <gouider.heidi@gmail.com>
2023-05-24 15:17:08 +0200

    enrg. modif validation formulaire js

--- evaluation_js/formulaire_js/assets/js/script.js
+++ evaluation_js/formulaire_js/assets/js/script.js
@@ -15,53 +15,108 @@ S'il manque une ou pls valeurs un message s'affichera.
 /************** TEST POUR LES PREMIERS CHAMPS OBLIGATOIRES AVEC UN CLICK SUR LE BOUTON VALIDER ******************** */
 
     // form.addEventListener("submit", function (event) {
-    form.valider.addEventListener("click", function (event) {
+form.valider.addEventListener("click", function (event) {
 // document.forms["contact"].addEventListener("submit", function (event) {
     event.preventDefault();
 
+/********* TEST BOUCLE ************/
 
 /*ici j'aimerais me servir d'une boucle (pour éviter les longueurs :) !) et vérifier les valeurs manquantes de chaque champs 
 obligatoires. Je n'ai pas encore trouvé la solution .... research going on ;)*/
 
-//     let inputs = document.getElementsByClassName("champ_obligatoire").getElementsByTagName("input");
-//     let inputs = this;
+/*     let inputs = document.getElementsByClassName("champ_obligatoire").getElementsByTagName("input");
+     let inputs = this;
 
-//     for (let i = 0; i < inputs.length; i++) {
+     for (let i = 0; i < inputs.length; i++) {
 
-//         if (inputs[i].valueMissing)
-//         // (inputs[i].validity.valueMissing) 
-//         {
-//             // inputs.style.border = " 1px solid red";
-//      console.log ("erreur");
-//         }
+         if (inputs[i].valueMissing)
+         // (inputs[i].validity.valueMissing) 
+         {
+             // inputs.style.border = " 1px solid red";
+      console.log ("erreur");
+         }
         
-//     };
-// });
+     };
+ }); */
   
+
+
+/* je selectionne ma balise titre vide dans mon document pour afficher un message à
+   l'U si le formulaire est envoyé*/
+    // const messageEnvoie = document.getElementById("succesEnvoie");
+
 /* je vais utiliser la propriété validity.valueMissing
    pour vérifier si les champ de saisie ne sont pas vide et renvoyer un message si c'est le cas.*/
 
+   // pour le prénom et le nom il faudra aussi créer un regExp que je pourrais mettre dans la condition avec un opérateur logique
+    let identityRegExp = new RegExp (
+    "^[a-zA-Z-]$"
+    // {2,10}
+    );
+    let testPrenom = identityRegExp.test(form.prenom);
 
+
+        // if ((form.prenom.validity.valueMissing) || (form.prenom!=identityRegExp)) {
         if (form.prenom.validity.valueMissing) {
             // e.preventDefault();
             form.prenom.style.border = " 1px solid red";
             prenomMiss.textContent = "Ce champ est obligatoire";
+        }
+        else if (form.prenom!==identityRegExp) {
+            form.prenom.style.border = " 1px solid red";
+            prenomMiss.textContent = "erreur de saisie";
         };
-        
+        // if (!testPrenom) {
+        //     form.prenom.style.border = " 1px solid red";
+        //     prenomMiss.textContent = "erreur de saisie";
+        // };
         if (form.nom.validity.valueMissing) {
             // e.preventDefault();
             form.nom.style.border = " 1px solid red";
             nomMiss.textContent = "Ce champ est obligatoire";
+        }
+        else if (form.nom!==identityRegExp) {
+            form.nom.style.border = " 1px solid red";
+            nomMiss.textContent = "erreur de saisie";
         };
 
-        // erreur de script pour cette condition !!!!! A corriger /nb.voir le propriété checked(balise html)
-        //  ici je laisse le commentaire car j'ai enfin trouvé la solution !! 
-        //  en plus de l'erreur d'oublie du checked, ma condition était : si ou ...  Bam trouvé!
+
+        /* Erreur de script pour cette condition !!!!! A corriger /nb.voir le propriété checked(balise html)
+        Ici je laisse le commentaire précédent, car j'ai enfin trouvé la solution !! Explication :
+
+        1-> Dans ma première ligne, j'ai utilisé la même propriété que pour les input. Sauf que là il sagit d'une checkbox.
+        2-> Pour ma condition, j'ai choisi l'opérateur logique "ou", d'ou le message d'erreur si les deux cases n'étaient pas cochées
+        
+        Résolution du problème-> Avec la bonne propriété et le bon opérateur logique =>  Bam trouvé! */
+
+        // if (form.feminin.validity.valueMissing) || (form.masculin.validity.valueMissing){
         if ((!form.feminin.checked ) && (!form.masculin.checked )) {
             // e.preventDefault();
             // form.feminin.style.border, form.masculin.style.border = " 1px solid red";
             sexeMiss.textContent = "Ce champ est obligatoire";
         };
+
+        if (form.naissance.validity.valueMissing) {
+            // e.preventDefault();
+            form.naissance.style.border = " 1px solid red";
+            naissanceMiss.textContent = "Ce champ est obligatoire";
+        };
+
+    // let cpRegExp = new RegExp(
+    //     "^[0-9]{5}$", "g"
+    // );
+    
+    //     if (form.cp.validity.valueMissing) {
+    //         // e.preventDefault();
+    //         form.naissance.style.border = " 1px solid red";
+    //         naissanceMiss.textContent = "Ce champ est obligatoire";
+    //     }
+    //     else if (form.cp!=cpRegExp) {
+    //         form.cp.style.border = " 1px solid red";
+    //         cpMiss.textContent = "erreur de saisie";
+    //     };
+
+
 
         // if (form.sujet) pour la liste déroulante, il faut s'assurer que l'U selectionne(click?) un de [2 à 5] de la liste
 
@@ -69,7 +124,7 @@ obligatoires. Je n'ai pas encore trouvé la solution .... research going on ;)*/
 
         // let validEmail = function (email) {
             let emailRegExp = new RegExp(
-            "^[a-zA-Z0-9;-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$", "g"
+            "^[a-z0-9;-_]+[@]{1}[a-z0-9.-_]+[.]{1}[a-z]{2,10}$", "g"
             );
 
         if(form.email != emailRegExp) {
@@ -78,11 +133,20 @@ obligatoires. Je n'ai pas encore trouvé la solution .... research going on ;)*/
             // small.classList.add = "text-danger";
             // return fasle;
         } 
+        // else if (form.email.validity.valueMissing) {
+        //     // e.preventDefault();
+        //     form.naissance.style.border = " 1px solid red";
+        //     naissanceMiss.textContent = "Ce champ est obligatoire";
         // };
 
-            
+        // };
+
+            // si le formaulaire est rempli correctement il sera envoyé et disparaîtra
+            // un message d'envoie s'affichera pour l'U.
+            // je selectionne le "titre" dans mon document 
         else {
             form.valider.setCustomValidity("");
+            // messageEnvoie.textContent = "Votre demande a été envoyé !";
         }
     });
 // });
